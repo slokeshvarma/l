f4744ffa78f5fef9c890eccb5fab9f32da102985
Anand <slokeshvarma117@gmail.com>
2025-03-09 10:36:12 +0530

    changed

--- main2.js
+++ main2.js
@@ -1,8 +1,16 @@
-URLProvider()
-
 function URLProvider() {
     const path = window.location.pathname.toLowerCase().split("/").pop();
+    
+    document.querySelectorAll("a").forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            
+            console.log('Link clicked, but default behavior prevented!');
+        });
+    });
+    
     if (path.includes("aboutus")) {
         console.log("hello");
     }
-}
+};
+URLProvider();
